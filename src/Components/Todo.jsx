87336b264d34c51ee4e09fcebe66975554ddf52f
kavindujay99 <kavinduduj32@gmail.com>
2024-10-07 03:03:33 +0530

    Added delete button functionality in To-Do List app

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { LuListTodo } from "react-icons/lu";
 import TodoItems from './TodoItems';
 
 
 const Todo = () => {
+
+    const [todoList,setTodoList] = useState([]);
+
+    const inputRef = useRef()
+
+    const add = ()=>{
+        const inputText = inputRef.current.value.trim();
+
+        if(inputText ===""){
+            return null;
+        }
+
+        const newTodo = {
+            id: Date.now(),
+            text: inputText,
+            isComplete: false,
+        }
+        setTodoList((prev)=>[...prev, newTodo]);
+        inputRef.current.value = "" 
+    }
+
+    const deleteTodo = (id) => {
+        setTodoList((prevTodos)=>{
+            return prevTodos.filter((todo) => todo.id !== id)
+        })
+    }
+
   return (
     <div className='bg-white place-self-center	w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
        
@@ -16,14 +43,17 @@ const Todo = () => {
 
         {/* -----Input-Box ----- */}
         <div className='flex items-center my-7 bg-gray-200 rounded-full'>
-            <input type="text" className="bg-transparent border-0 outline-none flex-1 h-10 pl-6 pr-2 placeholder:text-slate-600" placeholder='Add your Task'/>
-            <button className='border-none rounded-full bg-fuchsia-500 hover:bg-fuchsia-600  w-32 h-10 text-white text-lg font-medium cursor-pointer'>Add +</button>
+            <input ref={inputRef} type="text" className="bg-transparent border-0 outline-none flex-1 h-10 pl-6 pr-2 placeholder:text-slate-600" placeholder='Add your Task'/>
+            <button onClick={add} className='border-none rounded-full bg-fuchsia-500 hover:bg-fuchsia-600  w-32 h-10 text-white text-lg font-medium cursor-pointer'>Add +</button>
         </div>
 
         {/* ----- Todo-List ----- */}
 
         <div>
-            <TodoItems text="Learn"/>
+            {todoList.map((item, index)=>{
+                return <TodoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo}/>
+            })}
+            
         </div>
     </div>
   )
--- src/Components/TodoItems.jsx
+++ src/Components/TodoItems.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { GiCheckMark } from "react-icons/gi";
 import { RiDeleteBin5Line } from "react-icons/ri";
 
-const TodoItems = ({text}) => {
+const TodoItems = ({text, id, isComplete, deleteTodo}) => {
   return (
     <div className='flex items-center my-3 gap-2'>
         <div className='flex flex-1 items-center cursor-pointer'>
@@ -11,7 +11,7 @@ const TodoItems = ({text}) => {
             <p className='text-slate-700 ml-4 text-[17px]'>{text}</p>
 
         </div>
-        <RiDeleteBin5Line className='w-9 h-4 cursor-pointer hover:text-rose-600'/>
+        <RiDeleteBin5Line onClick={()=>{deleteTodo(id)}} className='w-9 h-4 cursor-pointer hover:text-rose-600'/>
     </div>
   )
 }
